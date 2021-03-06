10e9dd6297ee3bca302060689dfe012d537811e0
Lucas Ransan <lucas@ransan.tk>
2021-03-06 22:17:37 +0100

    basic blinking program

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,21 @@
+const DDRB = @intToPtr(*volatile u8, 0x24);
+const PORTB = @intToPtr(*volatile u8, 0x25);
+
 export fn main() noreturn {
-    while (true) {}
+    DDRB.* |= 1 << 5;
+    while (true) {
+        PORTB.* &= ~(@as(u8, 1 << 5));
+        var i: u32 = 0;
+        while (i < 500000) {
+            asm volatile ("nop");
+            i += 1;
+        }
+
+        PORTB.* |= 1 << 5;
+        i = 0;
+        while (i < 500000) {
+            asm volatile ("nop");
+            i += 1;
+        }
+    }
 }
